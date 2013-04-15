dbde3ed6d2c5c3da587f8eee734f0fa93c65551a
Frederico Spengler <spengler@gmail.com>
2013-04-14 22:59:21 -0300

    Initial Commit do Se Eu Mandasse No Brasil

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SeEuMandasseNoBrasil::Application.config.secret_token = '2cba3bc844a8052f504bccf5655d9250ae7198b394fc4a32c357d9d015edd6ac22e27fa6cdb9693ae204a8a9d8748687ec4061a22e271eb46cc4db0847ff45b9'
---
+++ vendor/bundle/gems/pg-0.15.1/ext/siteconf20130414-7840-znjlw9.rb
@@ -0,0 +1,6 @@
+require 'rbconfig'
+dest_path = "./.gem.20130414-7840-s22wzm"
+RbConfig::MAKEFILE_CONFIG['sitearchdir'] = dest_path
+RbConfig::CONFIG['sitearchdir'] = dest_path
+RbConfig::MAKEFILE_CONFIG['sitelibdir'] = dest_path
+RbConfig::CONFIG['sitelibdir'] = dest_path
